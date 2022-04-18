1c313ab4dacc5c0f4486b85b1d697a6d1b61f63b
SergeyPlatinov <slplatinov@yandex.ru>
2022-04-18 21:01:25 +0300

    решено лаболаторная №3 Лаболаторная №3; дан массив с элементами [2, 3, 4, 5]. С помощью цикла for найдите произведение элементов данного массива.

---
+++ src/lab_3/Lab_3_Aray.java
@@ -0,0 +1,21 @@
+package lab_3;
+
+public class Lab_3_Aray {
+    public static void main(String[] args) {
+        int sum = 1;
+        int j = 2;
+        int[] massive_lab3 =new int[4];
+      for (int i=0;i<4;i++){
+          massive_lab3[i] = j;
+          j++;
+              sum = sum*massive_lab3[i];
+
+      }
+        System.out.println(sum);
+    }
+
+
+
+
+
+}
